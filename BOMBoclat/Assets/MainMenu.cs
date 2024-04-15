72193e87303f1b10abbfc59f4610d8bc50d3ec09
ajdahcode <aj.bagarra@gmail.com>
2024-04-15 17:15:50 -0400

    move main menu setups

---
+++ BOMBoclat/Assets/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void GoSettings()
+    {
+        SceneManager.LoadScene("SettingsMenu");
+    }
+
+    public void GoMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
