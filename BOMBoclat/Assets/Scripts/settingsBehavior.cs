d4b71fb76e33aed2316f63d40a215014f4d63599
djanton1254 <dariojosetantonio@gmail.com>
2024-04-18 22:17:16 -0400

    main menu changes (not done)
    sum changes to the menu for volume settings

---
+++ BOMBoclat/Assets/Scripts/settingsBehavior.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class settingsBehavior : MonoBehaviour
+{
+    public Slider masterVol;
+    public AudioMixer masterAudioMixer;
+    public float masterFloat;
+    public void changeMasterVolume()
+    {
+        masterAudioMixer.SetFloat("MasterVol", masterVol.value);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SaveSoundSettings()
+    {
+        PlayerPrefs.SetFloat()
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
